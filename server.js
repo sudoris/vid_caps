c783b6d882c3e817bcc423264bbb120e6adfd5bd
David Chen <zdent09@gmail.com>
2020-06-14 13:31:37 +0800

    Set up server to listen on port 5555, add index.html skeleton

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const ffmpeg = require('fluent-ffmpeg')
 const fileUpload = require('express-fileupload')
 
 const app = express()
+const port = 5555
 
 ffmpeg.setFfmpegPath('C:/ffmpeg/bin/ffmpeg.exe')
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(port, () => {
+  console.log('Server listening on port: ' + port)
+})
